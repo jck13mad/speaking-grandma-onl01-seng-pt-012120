33e05b75a9bafc7a67d0a14d6274a3a588c2d2fe
Jack Madden <jck13mad@gmail.com>
2020-01-23 18:09:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"  
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
-  elsif string == string.downcase
+  else 
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
