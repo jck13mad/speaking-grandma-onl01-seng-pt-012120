0e77bf6cdba74c0e45af8104f3fb32ef6fe1412a
Jack Madden <jck13mad@gmail.com>
2020-01-23 18:08:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,7 @@ def speak_to_grandma(string)
     return "NO, NOT SINCE 1938!"
   elsif string == string.downcase
     return "HUH?! SPEAK UP, SONNY!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
